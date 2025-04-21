9107e009dfbc5cda78e74afa4433f4cdeaae3b44
basamba1990 <basamba1990@yahoo.fr>
2025-04-21 17:40:27 +0000

    Update whisper_utils.py

--- streamlit_app/whisper_utils.py
+++ streamlit_app/whisper_utils.py
@@ -1,9 +1,13 @@
+import streamlit as st
 import openai
-import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def transcribe_audio(file_path):
     with open(file_path, "rb") as audio_file:
-        transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    return transcript['text']
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+    return transcript.text
